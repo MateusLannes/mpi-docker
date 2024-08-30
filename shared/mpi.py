3bfc5f8c58646af98ef184611a6885b1f3db066d
Mateus <mateuslannes100@gmail.com>
2024-08-29 21:30:56 -0400

    Update mpi.py

--- shared/mpi.py
+++ shared/mpi.py
@@ -4,16 +4,11 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-# Process master
 if rank == 0:
     data = {'x': 1, 'y': 2.0}
-    # Master process sends data to worker processes by
-    # going through the ranks of all worker processes
-    for i in range(1, size):
+    for i in range (1, size):
         comm.send(data, dest=i, tag=i)
-    print(f'Process {rank} sent data:', data)
-# Worker processes
+        print('Process {} sent data:'.format(rank), data)
 else:
-    # Each worker process receives data from master process
     data = comm.recv(source=0, tag=rank)
-    print(f'Process {rank} received data:', data)
+    print('Process {} received data:'.format(rank), data)
